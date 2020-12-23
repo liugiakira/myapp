ce266e985d8b87a0eeda576485190e0a8dfcdbb8
kristina.yakovleva <kristina.yakovleva@jrholding.co>
2020-12-23 12:52:39 +0300

    fix

--- lib/planner/plannerapp.dart
+++ lib/planner/plannerapp.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import 'core/transaction.dart';
+import 'models/transaction.dart';
 
 class PlannerApp extends StatelessWidget {
   @override
